ec085c3c8b1ad9d0e8a4312c34a60ad07b5f206a
David Saperstein <d.h.saperstein@gmail.com>
2022-02-07 19:44:07 -0700

    toy made. need css

---
+++ client/src/App.jsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from 'react'
+
+function App() {
+
+  const initState = {
+    control: 0,
+    totalControl: 0,
+    trophies: 0,
+    deck: 0,
+    innerCircle: 0,
+    tokens: 0
+  }
+  
+  const [scores, setScores] = useState(initState)
+  const [finalScore, setFinalScore] = useState(0)
+  
+  function handleChange(e) {
+    e.preventDefault()
+    const {name, value} = e.target
+    setScores(prevScores => ({
+      ...prevScores,
+      [name]: value
+    }))
+  }
+  
+  function handleFinal() {
+    const scoreArray = Object.values(scores)
+    scoreArray.forEach(score => {
+      console.log(typeof score)
+    })
+    const total = scoreArray.reduce((acc, val) => {
+      return parseInt(acc) + parseInt(val)
+    })
+    setFinalScore(total)
+  }
+
+  function handleReset(e){
+    e.preventDefault()
+    setScores(initState)
+  }
+
+  useEffect(() => {
+    handleFinal()
+  }, [scores])
+
+
+  
+
+  return (
+    <div>
+      <form>
+        <label>
+          VP value of each site you control: 
+          <input 
+            type='number'
+            name='control'
+            value={scores.control}
+            onChange={handleChange}
+            min='0'
+            required
+          />
+        </label>
+
+        <label>
+          2 VP for each site under total control: 
+          <input 
+            type='number'
+            name='totalControl'
+            value={scores.totalControl}
+            onChange={handleChange}
+            min='0'
+            required
+          />
+        </label>
+
+        <label>
+          1 VP for each troop in your trophy hall:  
+          <input 
+            type='number'
+            name='trophies'
+            value={scores.trophies}
+            onChange={handleChange}
+            min='0'
+            required
+          />
+        </label>
+
+        <label>
+          Deck, hand and discard VP value: 
+          <input 
+            type='number'
+            name='deck'
+            value={scores.deck}
+            onChange={handleChange}
+            min='0'
+            required
+          />
+        </label>
+
+        <label>
+          Inner-circle VP value: 
+          <input 
+            type='number'
+            name='innerCircle'
+            value={scores.innerCircle}
+            onChange={handleChange}
+            min='0'
+            required
+          />
+        </label>
+
+        <label>
+          VP Tokens 
+          <input 
+            type='number'
+            name='tokens'
+            value={scores.tokens}
+            onChange={handleChange}
+            min='0'
+            required
+          />
+        </label>
+      </form>
+      <p>{finalScore}</p>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  )
+}
+
+export default App
